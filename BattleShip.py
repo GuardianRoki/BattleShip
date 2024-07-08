0a736ae366973dee26b4fbfc95203a26a645c68d
GuardianRoki <marc.primus27@gmail.com>
2024-07-08 10:37:40 -0400

    Add files via upload

---
+++ BattleShip.py
@@ -0,0 +1,17 @@
+
+
+def createBoard():
+    
+    board = [["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"]]
+    return board
+
+def main():
+
+    p1ShipList = []
+    p2ShipList = []
+    p1HitList = []
+    p2HitList = []
