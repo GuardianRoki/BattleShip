dcaf2fdac602298a31632cd655078669783d74f6
GitChase-c <chasecoxon2024@gmail.com>
2024-07-08 10:54:15 -0400

    Add files via upload

---
+++ Battle_Ship.py
@@ -0,0 +1,32 @@
+import random
+
+def createBoard():
+    
+    board = [["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"],
+            ["0","0","0","0","0"]]
+    return board
+
+def bombDropping(targetLoc, userBomb):
+    targetLoc = userBomb.split(",")
+    yPos = targetLoc[1]
+    xPos = targetLoc[0]
+
+
+userBomb = input("Please select a section to hit with your artilery")
+targetLoc = None
+
+def main():
+
+
+    p1ShipList = []
+    p2ShipList = []
+    p1HitList = []
+    p2HitList = []
+    return userBomb
+    return targetLoc
+
+
+
